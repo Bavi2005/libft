14366db655059dacbefd387015f3f6842f8aefad
Bavi2005 <bavithrran@gmail.com>
2025-05-27 00:40:43 +0800

    just a simple push

---
+++ libft code/ft_memchr.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Bavi </var/spool/mail/Bavi>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/27 00:19:26 by Bavi              #+#    #+#             */
+/*   Updated: 2025/05/27 00:31:36 by Bavi             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stddef.h>
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (((unsigned char *)s)[i] == (unsigned char)c)
+			return (((unsigned char *)s) + i);
+		i++;
+	}
+	return (NULL);
+}
+/*
+#include <stdio.h>
+
+int main() 
+{
+    const char *str = "Hello, World!";
+    char search_char = ' ';
+    size_t search_size = 13;
+
+    // Using ft_memchr to find the first occurrence of 'o' in str
+    void *result = ft_memchr(str, search_char, search_size);
+
+    if (result != NULL) {
+        printf("Found '%c' at position: %ld\n", search_char
+	, (char *)result - str);
+    } else {
+        printf("'%c' not found in '%s'\n", search_char, str);
+    }
+
+    return 0;
+}*/
